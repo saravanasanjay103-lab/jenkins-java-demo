210df2bf03cfc2dcb41d708cd183d78097cd460a
sanjaimk <saravanasanjay103@gmail.com>
2026-02-19 19:32:20 +0530

    trigger ci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Java Project!");
+        System.out.println("Hello CI World!");
     }
 }
